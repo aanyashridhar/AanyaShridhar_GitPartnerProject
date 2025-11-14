117962d773d282f8ee410db733febc191d9c696d
ahanasil <ahana.sil01@gmail.com>
2025-11-14 14:00:25 -0500

    Created SquareArray()
    Method that takes in an int and creates a square array of specified size, fills with random ints.

--- Aanya_Version.java
+++ Aanya_Version.java
@@ -1,3 +1,14 @@
 public class Aanya_Version {
-  
+	public static void main(String[] args){
+		SquareArray(10);
+	}
+  public static void SquareArray(int n){
+	  int[][] arr = new int[n][n];
+	  for(int i = 0; i < n; i++){
+		  for(int j = 0; j < n; j++){
+			  arr[i][j] = (int)(Math.random()*n);
+			  System.out.println(arr[i][j]);
+		  }
+	  }
+   }
 }
