9c6914b01380211ed6e8fd2da04eeb90308a20c5
ahanasil <ahana.sil01@gmail.com>
2025-11-14 14:19:14 -0500

    Created SumNeighborsGreater17()
    Method that takes in a 2D array, loops through to find even numbers, returns if sum of the neighbors of that number exceeds 17.

--- Aanya_Version.java
+++ Aanya_Version.java
@@ -1,7 +1,8 @@
 public class Aanya_Version {
 	public static void main(String[] args){
 		int[][] arr = SquareArray(10);
-		System.out.println("First even: " + firstEven(arr));
+		System.out.println("First even: " + FirstEven(arr));
+		System.out.println("First even w/ sum > 17: " + SumNeighborsGreater17(arr));
 	}
 	public static int[][] SquareArray(int n){
 		int[][] arr = new int[n][n];
@@ -13,14 +14,29 @@ public class Aanya_Version {
 		}
 		return arr;
 	}
-	public static int firstEven(int[][] arr){
-		int first = 0;
+	public static int FirstEven(int[][] arr){
 		for(int i = 0; i < arr.length; i++){
 			for(int j = 0; j < arr[0].length; j++){
 				if(arr[i][j] %2 == 0)
-					first = arr[i][j];
+					return arr[i][j];
 			}
 		}
-		return first;
+		return -1;
+	}
+	public static int SumNeighborsGreater17(int[][] arr){
+		int sum = 0;
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[0].length; j++){
+				if(arr[i][j] %2 == 0){
+					if(j != 0)
+						sum += arr[i][j - 1];
+					if(j != arr.length - 1)
+						sum += arr[i][j + 1];
+					if(sum > 17)
+						return arr[i][j];
+				}
+			}
+		}
+		return -1;
 	}
 }
