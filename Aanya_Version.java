269e27527e1b29fd0f5a2e0e1fba7f60944017a7
ahanasil <ahana.sil01@gmail.com>
2025-11-14 14:09:27 -0500

    Created firstEven()
    Method that takes in a 2D array and loops through, returning the first even number found.

--- Aanya_Version.java
+++ Aanya_Version.java
@@ -1,14 +1,26 @@
 public class Aanya_Version {
 	public static void main(String[] args){
-		SquareArray(10);
+		int[][] arr = SquareArray(10);
+		System.out.println("First even: " + firstEven(arr));
 	}
-  public static void SquareArray(int n){
-	  int[][] arr = new int[n][n];
-	  for(int i = 0; i < n; i++){
-		  for(int j = 0; j < n; j++){
-			  arr[i][j] = (int)(Math.random()*n);
-			  System.out.println(arr[i][j]);
-		  }
-	  }
-   }
+	public static int[][] SquareArray(int n){
+		int[][] arr = new int[n][n];
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				arr[i][j] = (int)(Math.random()*n);
+			System.out.println(arr[i][j]);
+			}
+		}
+		return arr;
+	}
+	public static int firstEven(int[][] arr){
+		int first = 0;
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[0].length; j++){
+				if(arr[i][j] %2 == 0)
+					first = arr[i][j];
+			}
+		}
+		return first;
+	}
 }
