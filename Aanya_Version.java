5a80a83eb9efc063164b16b6155190e17db28ffa
ahanasil <ahana.sil01@gmail.com>
2025-11-18 06:40:35 -0500

    Never Nested
    All code is limited to 3 indentations in (within their respective method).

--- Aanya_Version.java
+++ Aanya_Version.java
@@ -3,6 +3,7 @@ public class Aanya_Version {
 		int[][] arr = SquareArray(10);
 		System.out.println("First even: " + FirstEven(arr));
 		System.out.println("First even w/ sum > 17: " + SumNeighborsGreater17(arr));
+		System.out.println("First odd w/ sum > 33: " + FirstOddNeighborsSum33(arr));
 	}
 	public static int[][] SquareArray(int n){
 		int[][] arr = new int[n][n];
@@ -15,47 +16,39 @@ public class Aanya_Version {
 		return arr;
 	}
 	public static int FirstEven(int[][] arr){
-		for(int i = 0; i < arr.length; i++){
-			for(int j = 0; j < arr[0].length; j++){
-				if(arr[i][j] %2 == 0)
-					return arr[i][j];
-			}
-		}
+		for (int[] row : arr) {
+            for (int val : row) {
+                if (val % 2 == 0) return val;
+            }
+        }
 		return -1;
 	}
 	public static int SumNeighborsGreater17(int[][] arr){
-		int sum = 0;
-		for(int i = 0; i < arr.length; i++){
-			for(int j = 0; j < arr[0].length; j++){
-				if(arr[i][j] %2 == 0){
-					if(j != 0)
-						sum += arr[i][j - 1];
-					if(j != arr.length - 1)
-						sum += arr[i][j + 1];
-					if(sum > 17)
-						return arr[i][j];
-				}
-			}
-		}
+		for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[0].length; j++) {
+                int val = arr[i][j];
+                if (val % 2 == 0) {
+                    int sum = 0;
+                    if (j > 0) sum += arr[i][j - 1];
+                    if (j < arr[0].length - 1) sum += arr[i][j + 1];
+                    if (sum > 17) return val;
+                }
+            }
 		return -1;
 	}
 	public static int FirstOddNeighborsSum33(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr[0].length; j++) {
-
                 int val = arr[i][j];
-                if (val % 2 == 0) continue;
-
-                int sum = 0;
-
-                if (i > 0) sum += arr[i - 1][j];
-                if (i < arr.length - 1) sum += arr[i + 1][j];
-                if (j > 0) sum += arr[i][j - 1];
-                if (j < arr[0].length - 1) sum += arr[i][j + 1];
-
-                if (sum > 33) return val;
+                if (val % 2 != 0) {
+                    int sum = 0;
+                    if (i > 0) sum += arr[i - 1][j];
+                    if (i < arr.length - 1) sum += arr[i + 1][j];
+                    if (j > 0) sum += arr[i][j - 1];
+                    if (j < arr[0].length - 1) sum += arr[i][j + 1];
+                    if (sum > 33) return val;
+                }
             }
-        }
         return -1;
     }
 }
